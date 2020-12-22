794747f28dcb98faa7dd13f7f58034ea427d9025
eda0717 <playgirl1209@icloud.com>
2020-12-22 19:48:44 +0800

    first

---
+++ app/assets/js/all.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    //navbar滾動變色
+    $(window).scroll(function (e) {
+        if ($(window).scrollTop() <= 0) {
+            $('.navbar').addClass('bg-white');
+            $('.navbar').removeClass('bg-light');
+        } else {
+            $('.navbar').addClass('bg-light');
+            $('.navbar').removeClass('bg-white');
+        }
+    });
+
+
+
+
+
+})
